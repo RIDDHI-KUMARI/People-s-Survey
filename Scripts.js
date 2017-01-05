7b74157f98907e0c29faf176f36ad02f590aab73
RIDDHI-KUMARI <riddhilal14vdb@gmail.com>
2017-01-05 17:50:22 +0530

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,29 @@
+var app = angular
+.module("my Module" ,[])
+.controller("myController",function($scope){
+	var favouriteColour[
+	{name:red},
+	{name:blue},
+	{name:green},
+	{name:burgundy}
+];
+	var favouriteCuisine[
+	                	{name:Chinese},
+	                	{name:Mughlai},
+	                	{name:SouthIndian},
+	                	{name:Italian}
+	                ];
+	var favouriteMobile-OS[
+	                	{name:Android},
+	                	{name:IOS},
+	                	{name:Windows},
+	                	{name:Firefox-OS1}
+	                ];
+	var Popular brand[
+	                	{name:GSK},
+	                	{name:ITC},
+	                	{name:Pidlite},
+	                	{name:Amul}
+	                ];
+}
+)
